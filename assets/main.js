e6f6615f3adf51e1d4c97d66e633dcdbabf4b4cf
Débora Paulin <debipaulin@gmail.com>
2023-03-01 10:08:06 -0300

    adicionando mudança de turnos

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,36 @@
-const cellElements = document.querySelectorAll('{data-cell}');
+const cellElements = document.querySelectorAll('[data-cell]');
+const container = document.querySelector('[data-container]');
 
 let isCircleTurn = false;
 
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn
+
+    container.classList.remove('circle');
+    container.classList.remove('x');
+
+    if (isCircleTurn) {
+        container.classList.add('circle');
+    } else {
+        container.classList.add('x');
+    }
+};
+
 const handleClick = () => {
     // Colocar a marca (X ou Círculo)
-    const cell = e.target;
+    const cell = Event.target;
     const classToAdd = isCircleTurn ? 'circle' : 'x';
 
-    cell.classList.add(classToAdd);
+    placeMark(cell, classToAdd);
+
     // Verificar por vitória
     // Verificar por empate 
     // Mudar símbolo
+    swapTurns();
 };
 
 for (const cell of cellElements) {
